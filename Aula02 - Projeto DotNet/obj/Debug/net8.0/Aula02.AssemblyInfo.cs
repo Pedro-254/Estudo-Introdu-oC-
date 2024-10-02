d64e648240f04948fa7a294255d5cdc990c2e7c1
PEDRO LUCAS VIEIRA ADORNO <fei01@toledobrasil.com>
2024-10-02 09:43:39 -0300

    att

--- Aula02 - Projeto DotNet/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02 - Projeto DotNet/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777041db02d0a17ce7f78142979a39a82f0c00d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d63a54ddb366c67e1bd71a703b4def516e946e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
